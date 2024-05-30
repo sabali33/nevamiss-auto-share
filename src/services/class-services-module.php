9c3b3def8950722a4853cf5c06812afc1ebfbebc
Eliasu Abraman <e.abraman@inpsyde.com>
2024-05-30 01:05:41 +0000

    Add Services Module

--- src/services/class-services-module.php
+++ src/services/class-services-module.php
@@ -4,37 +4,34 @@ declare(strict_types=1);
 namespace Nevamiss\Services\Contracts;
 
 use Inpsyde\Modularity\Module\ExecutableModule;
+use Inpsyde\Modularity\Module\ModuleClassNameIdTrait;
 use Inpsyde\Modularity\Module\ServiceModule;
+use Nevamiss\Service\Settings;
+use Nevamiss\Services\Date;
+use Nevamiss\Services\Instant_Post_Manager;
+use Nevamiss\Services\Logger;
+use Nevamiss\Services\Schedule_Post_Manager;
+use Nevamiss\Services\Task_Runner;
 use Psr\Container\ContainerInterface;
-use Saas\Infrastructure\WP_Cron_Service;
 
 class Services_Module implements ServiceModule, ExecutableModule
 {
+    use ModuleClassNameIdTrait;
 
     public function run(ContainerInterface $container): bool
     {
         // TODO: Implement run() method.
     }
 
-    public function id(): string
-    {
-        // TODO: Implement id() method.
-    }
-
     public function services(): array
     {
         return [
-            Schedule_Collection::class =>
-            static function($container): Schedule_Collection {
-                $schedule_service = $container->get(WP_Cron_Service::class);
-                $schedules = $schedule_service->get_all();
-                return new Schedule_Collection($schedules);
-            },
-
-            Settings::class => static function(){
-                return new Settings();
-            }
-
+            Date::class => fn() => new Date(),
+            Logger::class => fn() => new Logger(),
+            Instant_Post_Manager::class => fn() => new Instant_Post_Manager(),
+            Schedule_Post_Manager::class => fn() => new Schedule_Post_Manager(),
+            Task_Runner::class => fn() => new Task_Runner(),
+            Settings::class => fn() => new Settings(),
         ];
     }
 }
