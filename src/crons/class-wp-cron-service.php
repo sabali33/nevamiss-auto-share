657e80bbb096abf43fc49b0538aceeed327a54a7
Eliasu Abraman <e.abraman@inpsyde.com>
2024-05-30 00:55:41 +0000

    Setup Cron service

---
+++ src/crons/class-wp-cron-service.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Nevamiss\Crons\Cron_Interface;
+
+class WP_Cron_Service implements Cron_Interface {
+
+    public function create_schedule(): bool
+    {
+        // TODO: Implement create_schedule() method.
+    }
+
+    public function update_schedule(): bool
+    {
+        // TODO: Implement update_schedule() method.
+    }
+
+    public function delete_schedule(): bool
+    {
+        // TODO: Implement delete_schedule() method.
+    }
+
+    public function schedule(mixed $id): bool
+    {
+        // TODO: Implement schedule() method.
+    }
+}
