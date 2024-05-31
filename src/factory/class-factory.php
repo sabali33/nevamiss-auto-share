35a99795cd69abe89738d03eff9113641611a7fa
Eliasu Abraman <e.abraman@inpsyde.com>
2024-05-31 18:41:27 +0000

    Create Factory class

---
+++ src/factory/class-factory.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nevamiss\Factory;
+
+use Nevamiss\Application\Not_Found_Exception;
+
+class Factory
+{
+    public function new( string $class_name, mixed ...$args)
+    {
+        if(!class_exists($class_name)){
+            throw new Not_Found_Exception("class '$class_name' does not exist");
+
+        }
+        return new $class_name($args);
+    }
+
+}
