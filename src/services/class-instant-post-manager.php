9c1eadd4ec6fc57848075565b2b4fe9e0a5fee69
Eliasu Abraman <e.abraman@inpsyde.com>
2024-05-31 18:36:22 +0000

    Implement instant share post feature

--- src/services/class-instant-post-manager.php
+++ src/services/class-instant-post-manager.php
@@ -4,17 +4,40 @@ declare(strict_types=1);
 
 namespace Nevamiss\Services;
 
+use Nevamiss\Domain\Covariance\Share_Response;
+use Nevamiss\Domain\Entities\Network_Account;
+use Nevamiss\Networks\Contracts\Network_Clients_Interface;
 use Nevamiss\Services\Contracts\Remote_Post_Interface;
 
 class Instant_Post_Manager implements Remote_Post_Interface {
 
-    public function run(int $task_id): void
+    public function __construct(
+        private readonly Network_Account $account,
+        private readonly Network_Clients_Interface           $network_client,
+        private readonly Post_Formatter $formatter
+    )
     {
-        // TODO: Implement run() method.
+
     }
 
-    public function post(int $task_id): void
+    /**
+     * @param int $id Post ID.
+     * @return Share_Response
+     */
+    public function run(int $id): Share_Response
     {
-        // TODO: Implement post() method.
+        $data = $this->formatter->format($id);
+
+        return $this->post($data);
     }
+
+    public function post(string $data): Share_Response
+    {
+        $client = $this->network_client;
+
+        $response = $client->post($data, $this->account);
+
+        return new Share_Response($response);
+    }
+
 }
--- src/services/class-services-module.php
+++ src/services/class-services-module.php
@@ -10,6 +10,7 @@ use Nevamiss\Service\Settings;
 use Nevamiss\Services\Date;
 use Nevamiss\Services\Instant_Post_Manager;
 use Nevamiss\Services\Logger;
+use Nevamiss\Services\Post_Formatter;
 use Nevamiss\Services\Schedule_Post_Manager;
 use Nevamiss\Services\Task_Runner;
 use Nevamiss\Services\WP_Cron_Service;
@@ -29,11 +30,11 @@ class Services_Module implements ServiceModule, ExecutableModule
         return [
             Date::class => fn(): Date => new Date(),
             Logger::class => fn(): Logger => new Logger(),
-            Instant_Post_Manager::class => fn(): Instant_Post_Manager => new Instant_Post_Manager(),
             Schedule_Post_Manager::class => fn(): Schedule_Post_Manager => new Schedule_Post_Manager(),
             Task_Runner::class => fn(): Task_Runner => new Task_Runner(),
             Settings::class => fn(): Settings => new Settings(),
-            WP_Cron_Service::class => fn(): WP_Cron_Service => new WP_Cron_Service()
+            WP_Cron_Service::class => fn(): WP_Cron_Service => new WP_Cron_Service(),
+            Post_Formatter::class => fn(): Post_Formatter => new Post_Formatter(),
         ];
     }
 }
