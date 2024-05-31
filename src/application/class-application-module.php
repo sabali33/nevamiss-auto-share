15207665af561a0a5def56da5e90d44e17e565d7
Eliasu Abraman <e.abraman@inpsyde.com>
2024-05-31 10:20:44 +0000

    Refactor code

--- src/application/class-application-module.php
+++ src/application/class-application-module.php
@@ -2,15 +2,13 @@
 
 namespace Nevamiss\Application;
 
-use Exception;
 use Inpsyde\Modularity\Module\ExecutableModule;
 use Inpsyde\Modularity\Module\ModuleClassNameIdTrait;
 use Inpsyde\Modularity\Module\ServiceModule;
 use Psr\Container\ContainerInterface;
 
-class Module implements ServiceModule, ExecutableModule
+class Application_Module implements ServiceModule, ExecutableModule
 {
-    const MINIMUM_PHP_VERSION = '8.0';
 
     use ModuleClassNameIdTrait;
 
@@ -20,39 +18,21 @@ class Module implements ServiceModule, ExecutableModule
             DB::class => static function(){
                 global $wpdb;
                 return new DB($wpdb);
+            },
+            Plugin::class => static function(ContainerInterface $container) {
+                return new Plugin($container->get(DB::class));
             }
         ];
     }
 
     public function run(ContainerInterface $container): bool
     {
-        register_activation_hook(
-         NEVAMISS_ROOT,
-            static function() use($container){
-                $this->activate($container);
-            }
+
+        register_deactivation_hook(
+            NEVAMISS_ROOT,
+            [$container->get(Plugin::class), 'deactivate']
         );
-        register_deactivation_hook(NEVAMISS_ROOT, static function() use($container){
-            $this->deactivate($container);
-        });
 
         return true;
-    }
-
-    /**
-     * @throws Exception
-     */
-    private function activate(ContainerInterface $container): void
-    {
-        // Check for required PHP version
-        if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
-            throw new Exception('The server PHP version is not compatible', );
-        }
-        $container->get(DB::class)->setup_tables();
-    }
-
-    public function deactivate(ContainerInterface $container)
-    {
-        $container->get(DB::class)->drop_tables();
     }
 }
