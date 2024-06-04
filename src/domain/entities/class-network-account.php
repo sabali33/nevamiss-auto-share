87b0885022764690b283815f477ba5c55ee6fb0c
Eliasu Abraman <e.abraman@inpsyde.com>
2024-06-04 00:08:10 +0000

    Update entities classes

--- src/domain/entities/class-network-account.php
+++ src/domain/entities/class-network-account.php
@@ -5,12 +5,29 @@ namespace Nevamiss\Domain\Entities;
 class Network_Account
 {
     public function __construct(
-        private readonly string $token,
-        private readonly string $network
+        private int    $id,
+        private string $name,
+        private string $remote_account_id,
+        private string $token,
+        private string $network
     )
     {
     }
 
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function remote_account_id(): string
+    {
+        return $this->remote_account_id;
+    }
+
+    public function id(): int
+    {
+        return $this->id;
+    }
     public function token(): string
     {
         return $this->token;
