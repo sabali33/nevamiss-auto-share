4507fe8d42abbede2a195b6371370d775e5fa65e
Eliasu Abraman <e.abraman@inpsyde.com>
2024-06-04 00:08:10 +0000

    Update entities classes

--- src/domain/entities/class-task.php
+++ src/domain/entities/class-task.php
@@ -4,5 +4,39 @@ namespace Nevamiss\Domain\Entities;
 
 class Task
 {
+    public function __construct(
+    private int                                 $id,
+    private string                              $class_identifier,
+    private array                               $parameters,
+        private ?int                            $schedule,
+        private ?string                         $status= 'pending',
+    )
+    {
+    }
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function class_identifier(): string
+    {
+        return $this->class_identifier;
+    }
+
+    public function parameters(): array
+    {
+        return $this->parameters;
+    }
+
+    public function status(): string
+    {
+        return $this->status;
+    }
+
+    public function schedule(): ?int
+    {
+        return $this->schedule;
+    }
 
 }
