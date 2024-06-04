ecd13ebe90bc73f6c490d9e94a929c416e3cc4f1
Eliasu Abraman <e.abraman@inpsyde.com>
2024-06-04 00:09:50 +0000

    Add schedule posting feature

---
+++ src/services/class-schedule-tasks-runner.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nevamiss\Services;
+
+use Exception;
+use Nevamiss\Domain\Factory\Factory;
+use Nevamiss\Domain\Repositories\Network_Account_Repository;
+use Nevamiss\Domain\Repositories\Task_Repository;
+
+class Schedule_Tasks_Runner {
+
+    public function __construct(
+        private Task_Repository            $task_repository,
+        private Network_Account_Repository $account_repository,
+        private Factory                    $factory,
+        private array                      $network_clients
+    )
+    {
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function run(int $schedule_id): void
+    {
+        do_action('schedule_task_begins', $schedule_id);
+
+        $active_task = $this->task_repository->get_all(['schedule_id' => $schedule_id]);
+
+        if(!$active_task){
+            return;
+        }
+        [
+            'class_identifier' => $class_name,
+            'parameter' => $parameters,
+        ] = $active_task[0];
+
+        $network_account = $this->account_repository->get($parameters['network_account_id']);
+
+        $network_client = $this->network_clients[$network_account->network()];
+
+        /**
+         * @var Network_Post_Manager $post_manager
+         */
+        $post_manager = $this->factory->new($class_name, $network_account, $network_client);
+
+        $post_manager->run($parameters['post_id']);
+
+        do_action('schedule_task_complete', $active_task[0]['id']);
+
+        sleep(2);
+
+        $this->run($schedule_id);
+    }
+
+    /**
+     * A callback function that runs where an individual task is completed
+     *
+     * @throws Exception
+     */
+    public function update_task(int $task_id): void
+    {
+        $this->task_repository->update($task_id, ['status' => 1]);
+    }
+}
