1214160efd526dd6b12ff88f2f5ab6bdf201211b
Eliasu Abraman <e.abraman@inpsyde.com>
2024-06-07 08:24:57 +0000

    Allow tables created on plugin activation

--- src/application/class-setup.php
+++ src/application/class-setup.php
@@ -5,27 +5,44 @@ declare(strict_types=1);
 namespace Nevamiss\Application;
 
 use Exception;
+use function Nevamiss\error_notice;
 
-class Plugin {
+class Setup {
 
     const MINIMUM_PHP_VERSION = '8.0';
     private static DB $db;
+    private static ?Setup $instance = null;
 
-    public function __construct( DB $db)
+    public function __construct( string $db)
     {
-        static::$db = $db;
+        global $wpdb;
+
+        static::$db = new $db($wpdb);
+    }
+
+    public static function instance(string $db): void
+    {
+        if(self::$instance){
+            return;
+        }
+        self::$instance = new self($db);
+
+        register_activation_hook(
+            NEVAMISS_ROOT,
+            [self::$instance, 'activate']
+        );
     }
 
     /**
      * @throws Exception
      */
-    public static function activate(): void
+    public function activate(): void
     {
         // Check for required PHP version
         if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
             throw new Exception("The server PHP version {PHP_VERSION} is not compatible", );
         }
-        static::$db->setup_tables();
+        self::$db->setup_tables();
     }
 
     public function deactivate(): void
