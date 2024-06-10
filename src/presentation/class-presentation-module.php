75199dc359cc63e32c1d48c305f8023570f0c734
Eliasu Abraman <e.abraman@inpsyde.com>
2024-06-10 02:04:40 +0000

    Factor presentation module

--- src/presentation/class-presentation-module.php
+++ src/presentation/class-presentation-module.php
@@ -22,43 +22,32 @@ class Presentation_Module implements ServiceModule, ExecutableModule {
 
 			Schedules_Page::class       => static function ( ContainerInterface $container ) {
 
-				return new Schedules_Page(
-					$container->get( Schedules_Table_List::class ),
-					'Schedules',
-					'admin.php?page=schedules',
-					'schedules',
-					9
-				);
+				return new Schedules_Page( $container->get( Schedules_Table_List::class ) );
 			},
 
 			Settings_Page::class        => static function ( ContainerInterface $container ) {
-				return new Settings_Page(
-					$container->get( Settings::class ),
-					'Settings',
-					'admin.php?page=settings',
-					'settings',
-					10
-				);
+				return new Settings_Page( $container->get( Settings::class ) );
 			},
-			Stats_Page::class           => static function ( ContainerInterface $container ) {
-				return new Stats_Page(
-					$container->get( Posts_Stats_Repository::class ),
-					'Stats',
-					'admin.php?page=stats',
-					'stats',
-					10
-				);
-			},
+
+			Stats_Page::class           => static fn ( ContainerInterface $container ) => new Stats_Page(
+				$container->get( Posts_Stats_Repository::class )
+			),
+
 			Schedule_View_Page::class   => function ( ContainerInterface $container ): Schedule_View_Page {
 
-				return new Schedule_View_Page(
+				return new Schedule_View_Page( $container->get( Schedule_Repository::class ) );
+			},
+			Schedule_Form::class        => function ( ContainerInterface $container ): Schedule_Form {
+
+				return new Schedule_Form(
 					$container->get( Schedule_Repository::class ),
-					'Schedule',
-					'admin.php?page=schedule',
-					'schedule',
-					10
+					$container->get( Network_Account_Repository::class ),
+					$container->get( Factory::class )
 				);
 			},
+
+			Auto_Share_Page::class      => fn (): Auto_Share_Page => new Auto_Share_Page( array() ),
+
 			Post_Meta::class            => function ( ContainerInterface $container ) {
 				return new Post_Meta(
 					$container->get( Factory::class ),
