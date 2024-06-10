dfd3c60ceb66fbef9b2fa2cc1a53f82a4a58e6f6
Eliasu Abraman <e.abraman@inpsyde.com>
2024-06-10 02:10:01 +0000

    Display new/edit schedule page

---
+++ resources/templates/schedule-form.php
@@ -0,0 +1,24 @@
+<?php
+
+use Nevamiss\Presentation\Components\Input_Fields\Input;
+use Nevamiss\Presentation\Pages\Schedule_Form;
+
+/**
+ * @var Schedule_Form $this
+ */
+
+?>
+
+<h1 class="wp-heading-inline">
+    <?php echo esc_html($this->title); ?>
+</h1>
+
+<div class="wrap">
+    <form method="post">
+        <?php wp_nonce_field('nevamiss_create_schedule'); ?>
+        <?php foreach ($this->fields() as $field): ?>
+            <p> <?php $this->render_field($field); ?> </p>
+        <?php endforeach; ?>
+        <input type="submit" value="<?php esc_attr_e('Create', 'nevamiss'); ?>">
+    </form>
+</div>
