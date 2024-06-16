92e11d4c689ebe7a0a413f969f9efa44afff66b3
Eliasu Abraman <e.abraman@inpsyde.com>
2024-06-16 22:00:21 +0000

    Improve select field component

--- src/presentation/components/input-fields/class-select-field.php
+++ src/presentation/components/input-fields/class-select-field.php
@@ -8,32 +8,35 @@ use Nevamiss\Presentation\Components\Renderable;
 use Nevamiss\Presentation\Utils;
 
 class Select_Field implements Renderable {
-    private array $attributes = [
-        'choices' => [],
-        'label' => 'Enter select label', 'nevamiss',
-        'id' => 'select-id'
-    ];
+	private array $attributes = array(
+		'choices' => array(),
+		'label'   => 'Enter select label',
+		'nevamiss',
+		'id'      => 'select-id',
+	);
 	/**
 	 * @inheritDoc
 	 * @throws \Exception
 	 */
 	public function render( $attributes = array() ): string {
-        $attributes = array_merge($this->attributes, $attributes);
+		$attributes = array_merge( $this->attributes, $attributes );
 
-        $input_attributes = [
-            'class' => $attributes['class'] ?? '',
-            'name' => $attributes['name'] ?? 'select_field',
-        ];
+		$input_attributes = array(
+			'class'    => $attributes['class'] ?? '',
+			'name'     => $attributes['name'] ?? 'select_field',
+			'id'       => $attributes['id'] ?? 'select-field',
+			'multiple' => isset( $attributes['multiple'] ) && $attributes['multiple'],
+		);
 
-        if(isset($attributes['custom_inputs'])){
-            $input_attributes = array_merge($input_attributes, $attributes['custom_inputs']);
-        }
+		if ( isset( $attributes['custom_inputs'] ) ) {
+			$input_attributes = array_merge( $input_attributes, $attributes['custom_inputs'] );
+		}
 
-		$input_attr                   = Utils::build_input_attr( $input_attributes );
+		$input_attr = Utils::build_input_attr( $input_attributes );
 
-        if(isset($attributes['multiple']) && $attributes['multiple']){
-            $attributes['value'] = $attributes['value'] ?? [];
-        }
+		if ( isset( $attributes['multiple'] ) && $attributes['multiple'] ) {
+			$attributes['value'] = $attributes['value'] ?? array();
+		}
 		ob_start();
 
 		include NEVAMISS_PATH . 'resources/templates/select-field.php';
