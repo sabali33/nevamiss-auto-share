a9e6b0d313976187fc4bd5f410cf58661f455b9e
Eliasu Abraman <e.abraman@inpsyde.com>
2024-06-16 21:59:23 +0000

    Create select field components

---
+++ resources/templates/select-field.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ *
+ * @var array{choices:array, value:mixed, label: string, id:string} $attributes
+ * @var string $input_attr
+ */
+?>
+
+<p class="description description-wide">
+    <label for="<?php esc_attr_e($attributes['id']); ?>">
+        <?php esc_html_e($attributes['label']); ?>
+    </label>
+</p>
+
+<select <?php echo $input_attr; ?> >
+    <?php foreach( $attributes['choices'] as $key => $choice): ?>
+        <?php $selected = $key === $attributes['value'] || (is_array($attributes['value']) && in_array($key, $attributes['value'])) ?
+            'selected': ''
+        ?>
+        <option
+            value="<?php esc_attr_e($key); ?>"
+            <?php esc_attr_e($selected); ?>
+        >
+            <?php  esc_html_e($choice) ?>
+        </option>
+    <?php endforeach; ?>
+</select>
