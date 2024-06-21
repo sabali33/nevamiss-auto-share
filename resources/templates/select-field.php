9fa9fa38afcede12958ec9175efd78803b8817b8
Eliasu Abraman <e.abraman@inpsyde.com>
2024-06-21 19:15:20 +0000

    Allow for multi-selection of select field

--- resources/templates/select-field.php
+++ resources/templates/select-field.php
@@ -11,18 +11,18 @@
     <label for="<?php esc_attr_e($attributes['id']); ?>">
         <?php esc_html_e($attributes['label']); ?>
     </label>
-</p>
 
-<select <?php echo $input_attr; ?> >
-    <?php foreach( $attributes['choices'] as $key => $choice): ?>
-        <?php $selected = $key === $attributes['value'] || (is_array($attributes['value']) && in_array($key, $attributes['value'])) ?
-            'selected': ''
-        ?>
-        <option
-            value="<?php esc_attr_e($key); ?>"
-            <?php esc_attr_e($selected); ?>
-        >
-            <?php  esc_html_e($choice) ?>
-        </option>
-    <?php endforeach; ?>
-</select>
+    <select <?php echo $input_attr; ?> >
+        <?php foreach( $attributes['choices'] as $key => $choice): ?>
+            <?php $selected = $key === $attributes['value'] || (is_array($attributes['value']) && in_array($key, $attributes['value'])) ?
+                'selected': ''
+            ?>
+            <option
+                value="<?php esc_attr_e($key); ?>"
+                <?php esc_attr_e($selected); ?>
+            >
+                <?php  esc_html_e($choice) ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+</p>
