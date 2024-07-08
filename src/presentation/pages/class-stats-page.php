5c2a1d61788bb662cc76da7e7dfde1b5b68d67e6
Eliasu Abraman <e.abraman@inpsyde.com>
2024-07-08 14:47:02 +0000

    Add page slug

--- src/presentation/pages/class-stats-page.php
+++ src/presentation/pages/class-stats-page.php
@@ -7,19 +7,15 @@ use Nevamiss\Domain\Repositories\Posts_Stats_Repository;
 class Stats_Page extends Page {
 
 	public const TEMPLE_PATH = 'templates/stats';
-	/**
-	 * @var true
-	 */
-	private bool $is_sub_page;
-
-	public function __construct(
-		Posts_Stats_Repository $stats,
-		string $title,
-		string $slug,
-		string $filename,
-		int $priority
-	) {
-		parent::__construct( $stats, $title, $slug, $filename, $priority );
-		$this->is_sub_page = true;
+	const SLUG               = 'nevamiss-stats';
+	public function __construct( Posts_Stats_Repository $stats ) {
+		parent::__construct(
+			$stats,
+			'Stats',
+			self::SLUG,
+			10,
+			Auto_Share_Page::SLUG,
+			true
+		);
 	}
 }
