76fda55993cb16055d241240d2cc3c8c77ec08db
Eliasu Abraman <e.abraman@inpsyde.com>
2024-07-08 14:32:24 +0000

    Refactor more code

--- src/networks/class-media-network-collection.php
+++ src/networks/class-media-network-collection.php
@@ -4,7 +4,19 @@ declare(strict_types=1);
 
 namespace Nevamiss\Networks;
 
-interface Media_Network_Collection {
+use Nevamiss\Networks\Contracts\Network_Clients_Interface;
 
+class Media_Network_Collection {
+
+	private array $networks = [];
+	public function register(string $network_slug, Network_Clients_Interface $client): void
+	{
+		$this->networks[$network_slug] = $client;
+	}
+
+	public function get(string $network_slug)
+	{
+		return $this->networks[$network_slug];
+	}
 
 }
