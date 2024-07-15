d11707bf52f92fb13cddd22c4f34f75c4c4a19ff
Eliasu Abraman <e.abraman@inpsyde.com>
2024-07-15 07:09:12 +0000

    Improve request error handling

--- src/services/class-http-request.php
+++ src/services/class-http-request.php
@@ -4,28 +4,30 @@ declare(strict_types=1);
 
 namespace Nevamiss\Services;
 
+use Exception;
+
 class Http_Request {
 	/**
-	 * @throws \Exception
+	 * @throws Exception
 	 */
 	public function get(string $url, array $args=[]): array
 	{
 		$response = wp_remote_get($url, $args);
 
 		if(is_wp_error($response)){
-			throw new \Exception($response->get_error_message());
+			throw new Exception($response->get_error_message());
 		}
 		$body = wp_remote_retrieve_body($response);
 
 		if(!$body){
-			throw new \Exception($response['response']['message']);
+			throw new Exception($response['response']['message']);
 		}
 
 		return json_decode($body, true);
 	}
 
 	/**
-	 * @throws \Exception
+	 * @throws Exception
 	 */
 	public function post(string $url, array $args): array
 	{
@@ -35,13 +37,13 @@ class Http_Request {
 			$args
 		);
 		if(is_wp_error($response)){
-			throw new \Exception($response->get_error_message());
+			throw new Exception($response->get_error_message());
 		}
 
 		$body = wp_remote_retrieve_body($response);
 
 		if(!$body && !in_array($response['response']['code'], [200, 201]) ){
-			throw new \Exception($response['response']['message']);
+			throw new Exception($response['response']['message']);
 		}
 
 		if(!$body && isset($args['headers']['Linkedin-Version'])){
@@ -52,7 +54,7 @@ class Http_Request {
 	}
 
 	/**
-	 * @throws \Exception
+	 * @throws Exception
 	 */
 	public function put(string $url, array $args)
 	{
@@ -61,7 +63,7 @@ class Http_Request {
 			wp_parse_args( $args, ['method' => 'PUT'])
 		);
 		if(is_wp_error($response)){
-			throw new \Exception($response->get_error_message());
+			throw new Exception($response->get_error_message());
 		}
 
 		return json_decode(wp_remote_retrieve_body($response), true);
