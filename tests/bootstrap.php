776d88d58644f941a0e51dbcb999cba5a665c3f3
Eliasu Abraman <e.abraman@inpsyde.com>
2024-08-15 13:05:10 +0000

    Refactor bootstrapping tests files

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use function Patchwork\redefine;
+
+$vendor = dirname(__DIR__) . '/vendor/';
+
+if (!file_exists($vendor . 'autoload.php')) {
+	die('Please install via Composer before running tests.');
+}
+
+require_once $vendor . 'antecedent/patchwork/Patchwork.php';
+require_once $vendor . 'autoload.php';
+unset($vendor);
+
